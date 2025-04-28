55d907ac9b4a80e61a7fd556ae780482c06155b4
Ardox <ardox@axos-project.com>
2025-04-28 11:05:05 +0200

    changed rss slug

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,11 +4,11 @@ import rss from "@astrojs/rss";
 export async function GET(context) {
         const posts = await getCollection("blog");
         return rss({
-                title: `AxOS`,
+                title: "AxOS",
                 description: "AxOS - The Linux experience, enhanced.",
                 site: context.site,
                 items: posts.map((post) => ({
-                        link: post.slug,
+                        link: `/blog/${post.slug}`,
                         title: post.data.title,
                         description: post.data.snippet,
                         pubDate: post.data.publishDate,
